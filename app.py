0f67c2af56455d7e90e6de5644b85e2c2ce73217
Afrid-Basha01 <ma24csb0a47@student.nitw.ac.in>
2025-10-19 19:36:58 +0530

    Added SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask,render_template,request,session
+from sqlalchemy import create_engine
+
+# Path to your .mdf file
+db_path = r"D:\Java\JavaProject\Database\EvTracker.mdf"
 
 app=Flask(__name__)
 
 @app.route('/')
 def login():
     return render_template("index.html")
+
+@app.route('/request')
+def request():
+    pass
